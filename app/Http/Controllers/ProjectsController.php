94586f75a94a393a2b3266b48200973358968246
Velu Ilayaraja <velu76@gmail.com>
2017-04-20 02:06:23 +0800

    Add 'auth' to project edit

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -9,6 +9,11 @@ use Yajra\Datatables\Datatables;
 
 class ProjectsController extends Controller
 {
+    public function __construct() 
+    {
+        $this->middleware('auth');
+    }
+
     //
     public function index() 
     {
